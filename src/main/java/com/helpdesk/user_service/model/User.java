cab1e9a2b4298b4d2f375aaeece94160041aefe9
sheldon.carvalho <ratchubadasratchufly@gmail.com>
2026-08-24 18:34:44 -0300

    fix(db): alter the name of Entity and Table

---
+++ src/main/java/com/helpdesk/user_service/model/User.java
@@ -0,0 +1,43 @@
+package com.helpdesk.user_service.model;
+
+import com.helpdesk.user_service.role.UserRole;
+
+import java.time.LocalDateTime;
+
+import javax.annotation.processing.Generated;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import lombok.*;
+
+@Entity
+@Table(name = "user")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class User{
+    
+    //DLL -> Data Definition Language
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private UserRole role;
+
+    @Column(nullable = false)
+    private boolean active;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+}
