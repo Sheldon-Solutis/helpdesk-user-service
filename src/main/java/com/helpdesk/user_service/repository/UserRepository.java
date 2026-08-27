6df43705a14f93287d8f46287b3820b0d2eb11db
sheldon.carvalho <ratchubadasratchufly@gmail.com>
2026-08-27 09:41:14 -0300

    feat: repository find users functions

--- src/main/java/com/helpdesk/user_service/repository/UserRepository.java
+++ src/main/java/com/helpdesk/user_service/repository/UserRepository.java
@@ -3,5 +3,12 @@ package com.helpdesk.user_service.repository;
 import com.helpdesk.user_service.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserRepository extends JpaRepository<User, Long>{
+
+    List<User> findAllByActiveTrue();
+    List<User> findByIdAndActiveTrue(Long id);
+    List<User> findByIdAndActiveFalse(Long id);
+    List<User> findAllActiveFalse();
 }
